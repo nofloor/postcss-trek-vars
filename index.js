03767156cb2f32937e14b8ef264744a9a346a332
Taha Kelesoglu <taha@TahaK.local>
2020-09-17 19:27:14 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function trekVars(options = {}) {
+  return {
+    postcssPlugin: 'postcss-trek-vars',
+    Declaration(decl) {
+      if (decl.value.indexOf('-trek') > -1) {
+        const regex = /-trek-([a-z-]+)/;
+        const match = decl.value.match(regex);
+        if (match[1]) decl.value = decl.value.replace(regex, options[match[1]]);
+      }
+    }
+  }
+}
+
+trekVars.postcss = true;
+
+export default trekVars;
