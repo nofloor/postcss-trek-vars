5b14201da36a7e04eea5e705757d4bbf69901019
Taha Kelesoglu <taha.kelesoglu@gmail.com>
2020-09-29 12:55:23 +0300

    added calculation function

--- index.js
+++ index.js
@@ -2,13 +2,37 @@ function trekVars(options = {}) {
   return {
     postcssPlugin: 'postcss-trek-vars',
     Declaration(decl) {
-      if (decl.value.indexOf('-trek') > -1) {
-        const regex = /-trek-([a-z-]+)/;
+      let value = replaceVars(decl.value);
+      const regex = /(\s|^)(\(.+\))/;
+      if (regex.test(value)) {
         const match = decl.value.match(regex);
-        if (match[1]) decl.value = decl.value.replace(regex, options[match[1]]);
+        value = calculate(match[2]);
+        console.log(value);
       }
+      decl.value = value;
+      console.log(decl.value);
     }
   }
+  
+  function replaceVars(string) {
+    if (string.indexOf('-trek') > -1) {
+      const regex = /-trek-([a-z-]+)/g;
+      return string.replace(regex, (whole, variable) => undefined !== options[variable] ? options[variable] : whole);
+    }
+    return string;
+  }
+  
+  function calculate(string) {
+    const regex = /(\(|\s|^)+(\(.+\))(\s|;|$)?/;
+    const match = string.match(/([a-z]+)/g);
+    let unit = '';
+    if (undefined !== match[0]) {
+      unit = match[0];
+      string = string.replace(/([a-z]+)/g, '');
+    }
+    return eval(string) + unit;
+  }
+  
 }
 
 trekVars.postcss = true;
