1e4154dd75ab666aa01a79c7181f86a76cf7cf8f
Taha Kelesoglu <taha.kelesoglu@gmail.com>
2020-10-21 00:27:35 +0300

    added undefined return

--- index.js
+++ index.js
@@ -7,21 +7,10 @@ function trekVars(options = {}) {
   }
   
   function replaceVars(string) {
-    if (string.indexOf('-trek(') > -1) {
-      const regex = /\-trek\((.+)\)/g;
-      return string.replace(regex, (whole, match) => {
-        const vars = match.replace(/\s\s?/g, '').split(',');
-        for (let i = 0, count = vars.length; i < count; i++) {
-          if (undefined !== options[vars[i]]) return options[vars[i]];
-        }
-        return vars[vars.length - 1];
-      });
-    }
-    else if (string.indexOf('-trek-') > -1) {
+    if (string.indexOf('-trek-') > -1) {
       const regex = /-trek-([a-zA-Z0-9-_]+)/g;
-      return string.replace(regex, (whole, variable) => undefined !== options[variable] ? options[variable] : whole);
+      return string.replace(regex, (whole, variable) => undefined !== options[variable] ? options[variable] : 'undefined');
     }
-    
     return string;
   }
 }
--- index.test.js
+++ index.test.js
@@ -35,36 +35,12 @@ test('replaces trek variables', () => {
   decl.value = '0 (-trek-line-height / 2)';
   trekVars.Declaration(decl);
   expect(decl.value).toBe('0 (1.5 / 2)');
-});
 
-test('uses next variable if first does not exist', () => {
-  const decl = { value: '' };
-  
-  decl.value = '-trek(color, Black)';
+  decl.value = '-trek-gray-dark';
   trekVars.Declaration(decl);
-  expect(decl.value).toBe('#000000');
-  
-  decl.value = '-trek(color, Black, color_1)';
+  expect(decl.value).toBe('undefined');
+
+  decl.value = '1px solid -trek-primary-color';
   trekVars.Declaration(decl);
-  expect(decl.value).toBe('#000000');
-  
-  decl.value = '-trek(color, black, color_1, col)';
-  trekVars.Declaration(decl);
-  expect(decl.value).toBe('#000');
-  
-  decl.value = '-trek(  color, black  , color_1, col  )';
-  trekVars.Declaration(decl);
-  expect(decl.value).toBe('#000');
-  
-  decl.value = '-trek(padding-left, padding-right, 25px)';
-  trekVars.Declaration(decl);
-  expect(decl.value).toBe('25px');
-  
-  decl.value = '-trek(padding-left, padding-right, font-size)';
-  trekVars.Declaration(decl);
-  expect(decl.value).toBe('1rem');
-  
-  decl.value = '-trek(padding-left, padding-right, margin-left)';
-  trekVars.Declaration(decl);
-  expect(decl.value).toBe('margin-left');
+  expect(decl.value).toBe('1px solid undefined');
 });
